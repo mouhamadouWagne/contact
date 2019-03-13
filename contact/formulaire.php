b0887f4bd69f6187951d9446cc1ee5b0602bbdf2
=Mamoudouwagne <=wagnebaba10@gmail.com>
2019-03-13 23:44:57 +0100

    ajout des champs*

---
+++ contact/formulaire.php
@@ -0,0 +1,10 @@
+<?php
+echo '<img src ="mw.png"'.'<br/>';
+$personne=array('nom'=>'wagne' , 'prenom'=>'mouhamadou' , 'adresse'=>'aere lao' , 'profession'=>'etudiant' , 'tel'=>'782424178')
+foreach ($personne as $key => $value) {
+
+	echo '' . $key. ' . $value .
+}
+
+
+?>
